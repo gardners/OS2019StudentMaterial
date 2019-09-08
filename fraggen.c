a243e2dd335fb321cf983aec32c2c32cce24a7d4
Paul Gardner-Stephen <paul@servalproject.org>
2019-09-08 21:28:03 +0930

    fix regression

--- fraggen.c
+++ fraggen.c
@@ -834,8 +834,14 @@ int generate_assignment(char *left, char *right,int comparison_op,char *branch_t
       if (r->deref) printf("ldy #%d\n",reverse_order?(valid_bytes-1):0);
       break;
     case 1:
-      if (l->derefidx&&!l->derefidx->reg_y)
-	printf("ldy #%d\n",reverse_order?(valid_bytes-1):0);
+      {
+	int show=0;
+	if ((l->derefidx&&!l->derefidx->reg_y)) show=1;
+	if (!l->derefidx) show=1;
+	if ((r->derefidx&&!r->derefidx->reg_y)) show=1;
+	if (!r->derefidx) show=1;
+	if (show) printf("ldy #%d\n",reverse_order?(valid_bytes-1):0);
+      }
       break;
     case 2:
       if (l->derefidx&&!l->early_deref) {
